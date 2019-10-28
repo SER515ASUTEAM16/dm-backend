78d3fb44447e7ab697dd30eefee4d93eba43bc29
Vineeth Reddy <vineethreddy.agusta@gmail.com>
2019-10-28 05:58:25 -0800

    Working demonstrable application

--- src/main/java/com/doodlemath/dmbackend/Controller/UserController.java
+++ src/main/java/com/doodlemath/dmbackend/Controller/UserController.java
@@ -1,4 +1,56 @@
 package com.doodlemath.dmbackend.Controller;
 
+import com.doodlemath.dmbackend.Model.User;
+import com.doodlemath.dmbackend.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/auth")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/register")
+    public @ResponseBody
+    String addNewUser(@RequestBody User user) {
+        if (user.getName() == null || user.getName().isEmpty() ||
+                user.getPassword() == null || user.getPassword().isEmpty() ||
+                user.getEmail() == null || user.getEmail().isEmpty() ||
+                user.getRole() == null || user.getRole().isEmpty())
+            return "Failed";
+
+        userRepository.save(user);
+        return "Saved";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody
+    Iterable<User> getAllUsers() {
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
+    }
+
+    @PostMapping(path = "/loginByName")
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody
+    String loginByName(@RequestBody User user) {
+        if (user.getName() == null || user.getPassword() == null)
+            return "failed";
+        userRepository.findUserByName(user.getName(), user.getPassword());
+        return "Saved";
+    }
+
+    @PostMapping(path = "/loginByEmail")
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody
+    String loginByEmail(@RequestBody User user) {
+        if (user.getPassword() == null || user.getEmail() == null)
+            return "failed";
+        userRepository.findUserByEmail(user.getEmail(), user.getPassword());
+        return "Saved";
+    }
 }
--- src/main/java/com/doodlemath/dmbackend/Model/User.java
+++ src/main/java/com/doodlemath/dmbackend/Model/User.java
@@ -1,4 +1,62 @@
 package com.doodlemath.dmbackend.Model;
 
-public class User {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
