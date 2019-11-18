de7bb87f9d00d038b56a463cd29ff9d81139a9e3
Vineeth Reddy <vineethreddy.agusta@gmail.com>
2019-11-17 17:41:24 -0700

    Create Assignment by each author and search based on author

--- src/main/java/com/doodlemath/dmbackend/Controller/TeacherController.java
+++ src/main/java/com/doodlemath/dmbackend/Controller/TeacherController.java
@@ -1,16 +1,18 @@
 package com.doodlemath.dmbackend.Controller;
 
+import com.doodlemath.dmbackend.Model.Assignment;
+import com.doodlemath.dmbackend.Repository.AssignmentRepository;
+import com.doodlemath.dmbackend.constants.Constants;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.doodlemath.dmbackend.Model.User;
 import com.doodlemath.dmbackend.Repository.UserRepository;
-import com.doodlemath.dmbackend.constants.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /***
@@ -24,6 +26,9 @@ public class TeacherController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AssignmentRepository assignmentRepository;
+
     @CrossOrigin
     @GetMapping(path = "/getAllStudentsInGrade/{grade}")
     public @ResponseBody
@@ -32,10 +37,47 @@ public class TeacherController {
     }
 
     @CrossOrigin
-    @GetMapping(path = "/students")
+    @GetMapping(path = "/registerStudent")
     public @ResponseBody
     Iterable<User> addStudentToCourse(@RequestBody User user) {
-
         return userRepository.findAll();
+    }
+
+    @CrossOrigin
+    @PostMapping(path = "/createAssignment")
+    public @ResponseBody
+    String createAssignment(@RequestBody Assignment assignment) {
+        if(assignment.getAuthor() == null || assignment.getAuthor().isEmpty())
+            return Constants.FAILED;
+
+        if(assignment.getName() == null || assignment.getName().isEmpty())
+            return Constants.FAILED;
+
+        assignmentRepository.save(assignment);
+
+        return Constants.SUCCESS;
+    }
+
+    @CrossOrigin
+    @GetMapping(path = "/getAssignments/{authorName}")
+    public @ResponseBody
+    List<Assignment> getAssignments(@PathVariable String authorName) {
+        List<Assignment> assignment = assignmentRepository.findAssignmentsCreatedByAuthor(authorName);
+        return assignment;
+    }
+
+    @CrossOrigin
+    @GetMapping(path = "/getAllAssignments")
+    public @ResponseBody
+    List<Assignment> getAssignments() {
+        List<Assignment> assignmentList = new ArrayList<>();
+        Iterable<Assignment> assignments = assignmentRepository.findAll();
+
+        Iterator<Assignment> iterator = assignments.iterator();
+        while(iterator.hasNext()) {
+            assignmentList.add(iterator.next());
+        }
+
+        return assignmentList;
     }
 }
--- src/main/java/com/doodlemath/dmbackend/Model/Assignment.java
+++ src/main/java/com/doodlemath/dmbackend/Model/Assignment.java
@@ -1,10 +1,10 @@
 package com.doodlemath.dmbackend.Model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Table(name="assignment")
 public class Assignment implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -15,7 +15,59 @@ public class Assignment implements Serializable {
 
     private String name;
 
+    private String description;
+
     private String content;
 
-    private String status;
+    private String author;
+
+    private String creationDateTime;
+
+    public String getCreationDateTime() {
+        return creationDateTime;
+    }
+
+    public void setCreationDateTime(String creationDateTime) {
+        this.creationDateTime = creationDateTime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
