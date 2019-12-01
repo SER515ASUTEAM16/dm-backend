a093fe4996d7e3ca54fbf7330ea70b6740cc1677
Vineeth Reddy <vineethreddy.agusta@gmail.com>
2019-11-30 20:02:49 -0700

    Updated unit tests and readme

--- src/test/java/com/doodlemath/dmbackend/Controller/TeacherControllerTest.java
+++ src/test/java/com/doodlemath/dmbackend/Controller/TeacherControllerTest.java
@@ -1,6 +1,9 @@
 package com.doodlemath.dmbackend.Controller;
 
+import com.doodlemath.dmbackend.Model.User;
+import com.doodlemath.dmbackend.Repository.StudentTeacherRepository;
 import com.doodlemath.dmbackend.Repository.UserRepository;
+import com.doodlemath.dmbackend.constants.Constants;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +13,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,26 +24,44 @@ public class TeacherControllerTest {
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private StudentTeacherRepository studentTeacherRepository;
+
     @InjectMocks
     TeacherController teacherController;
 
+    List<User> students;
+
     @Before
     public void setup() {
+        students = new ArrayList<>();
 
+        //Registered 2 students
+        students.add(new User());
+        students.add(new User());
     }
 
     @Test
     public void getAllStudents() throws Exception {
+        //Return students in a particular grade
+        when(userRepository.findAllStudents(Mockito.any())).thenReturn(students);
+
+        Assert.assertNotNull(teacherController.getAllStudents("1-5"));
+    }
+
+    @Test
+    public void testWhenNoUserIsRegisteredReturnNull() throws Exception {
+        //When repository doesn't have students do not return any students
         when(userRepository.findAllStudents(Mockito.any())).thenReturn(null);
         Assert.assertNull(teacherController.getAllStudents("1-5"));
     }
 
     @Test
     public void addStudentToCourse() throws Exception {
-    }
+        when(studentTeacherRepository.save(Mockito.any())).thenReturn(null);
 
-    @Test
-    public void addStudentToCourse1() throws Exception {
+        //Return success upon adding students to the course
+        Assert.assertEquals(teacherController.addStudentToCourse("teacherName", students), Constants.SUCCESS);
     }
 
     @Test
--- src/test/java/com/doodlemath/dmbackend/DmBackendApplicationTests.java
+++ src/test/java/com/doodlemath/dmbackend/DmBackendApplicationTests.java
@@ -1,16 +1,12 @@
-//package com.doodlemath.dmbackend;
-//
-//import Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class DmBackendApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
+package com.doodlemath.dmbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class DmBackendApplicationTests {
+
+//	Untestable main method
+
+}
